0a75822bc913463c08588e8ef92935aed2dabc21
yyj <yangyujing@uniauto.me>
2018-01-04 11:20:40 +0800

    删除别名等函数

--- example/main/java/cn/jpush/api/examples/DeviceExample.java
+++ example/main/java/cn/jpush/api/examples/DeviceExample.java
@@ -9,51 +9,52 @@ import cn.jiguang.common.resp.APIConnectionException;
 import cn.jiguang.common.resp.APIRequestException;
 import cn.jiguang.common.resp.DefaultResult;
 import cn.jpush.api.JPushClient;
+import cn.jpush.api.device.AliasDeviceListResult;
 import cn.jpush.api.device.OnlineStatus;
 import cn.jpush.api.device.TagAliasResult;
 
 public class DeviceExample {
 	protected static final Logger LOG = LoggerFactory.getLogger(DeviceExample.class);
 
-	private static final String appKey = "dd1066407b044738b6479275";
-	private static final String masterSecret = "e8cc9a76d5b7a580859bcfa7";
+	private static final String appKey = "bc3e3f625657139c672774fc";
+	private static final String masterSecret = "541028d05f453d728595670e";
 	private static final String TAG1 = "tag1";
-	private static final String ALIAS1 = "alias1";
-	private static final String ALIAS2 = "alias2";
-	private static final String REGISTRATION_ID1 = "0900e8d85ef";
-	private static final String REGISTRATION_ID2 = "0a04ad7d8b4";
+	private static final String ALIAS1 = "868153020188221";
+	private static final String REGISTRATION_ID1 = "18071adc03318d49fc4";
 
 	private static JPushClient jpushClient = new JPushClient(masterSecret, appKey);
 
 	public static void main(String[] args) {
-		testGetDeviceTagAlias();
-//		testGetUserOnlineStatus();
+		// testGetDeviceTagAlias();
+		// testGetUserOnlineStatus();
+//		deleteAlias();// 删除别名
+//		getAliasDeviceList();
+		getDeviceTagAlias();
 	}
-	
+
 	public static void testGetDeviceTagAlias() {
 		try {
 			TagAliasResult result = jpushClient.getDeviceTagAlias(REGISTRATION_ID1);
-			
+
 			LOG.info(result.alias);
 			LOG.info(result.tags.toString());
-			
+
 		} catch (APIConnectionException e) {
 			LOG.error("Connection error. Should retry later. ", e);
-			
+
 		} catch (APIRequestException e) {
 			LOG.error("Error response from JPush server. Should review and fix it. ", e);
-            LOG.info("HTTP Status: " + e.getStatus());
-            LOG.info("Error Code: " + e.getErrorCode());
-            LOG.info("Error Message: " + e.getErrorMessage());
+			LOG.info("HTTP Status: " + e.getStatus());
+			LOG.info("Error Code: " + e.getErrorCode());
+			LOG.info("Error Message: " + e.getErrorMessage());
 		}
 	}
 
 	public static void testGetUserOnlineStatus() {
 		try {
-			Map<String, OnlineStatus> result =  jpushClient.getUserOnlineStatus(REGISTRATION_ID1, REGISTRATION_ID2);
+			Map<String, OnlineStatus> result = jpushClient.getUserOnlineStatus(REGISTRATION_ID1);
 
 			LOG.info(result.get(REGISTRATION_ID1).toString());
-			LOG.info(result.get(REGISTRATION_ID2).toString());
 		} catch (APIConnectionException e) {
 			LOG.error("Connection error. Should retry later. ", e);
 		} catch (APIRequestException e) {
@@ -66,7 +67,7 @@ public class DeviceExample {
 
 	public static void testBindMobile() {
 		try {
-			DefaultResult result =  jpushClient.bindMobile(REGISTRATION_ID1, "13000000000");
+			DefaultResult result = jpushClient.bindMobile(REGISTRATION_ID1, "13000000000");
 			LOG.info("Got result " + result);
 		} catch (APIConnectionException e) {
 			LOG.error("Connection error. Should retry later. ", e);
@@ -77,7 +78,48 @@ public class DeviceExample {
 			LOG.info("Error Message: " + e.getErrorMessage());
 		}
 	}
-	
+
+	// 删除别名
+	public static void deleteAlias() {
+		try {
+			DefaultResult result = jpushClient.deleteAlias(ALIAS1, "android");
+			LOG.info("Got result " + result);
+		} catch (APIConnectionException e) {
+			LOG.error("Connection error. Should retry later. ", e);
+		} catch (APIRequestException e) {
+			LOG.error("Error response from JPush server. Should review and fix it. ", e);
+			LOG.info("HTTP Status: " + e.getStatus());
+			LOG.info("Error Code: " + e.getErrorCode());
+			LOG.info("Error Message: " + e.getErrorMessage());
+		}
+	}
+
+	// 获取用户别名
+	public static void getDeviceTagAlias() {
+		try {
+			TagAliasResult result = jpushClient.getDeviceTagAlias(REGISTRATION_ID1);
+			LOG.info("Got result " + result);
+		} catch (APIConnectionException e) {
+			LOG.error("Connection error. Should retry later. ", e);
+		} catch (APIRequestException e) {
+			LOG.error("Error response from JPush server. Should review and fix it. ", e);
+			LOG.info("HTTP Status: " + e.getStatus());
+			LOG.info("Error Code: " + e.getErrorCode());
+			LOG.info("Error Message: " + e.getErrorMessage());
+		}
+	}
+	//
+	public static void getAliasDeviceList() {
+		try {
+			AliasDeviceListResult result = jpushClient.getAliasDeviceList(ALIAS1,"android");
+			LOG.info("Got result " + result);
+		} catch (APIConnectionException e) {
+			LOG.error("Connection error. Should retry later. ", e);
+		} catch (APIRequestException e) {
+			LOG.error("Error response from JPush server. Should review and fix it. ", e);
+			LOG.info("HTTP Status: " + e.getStatus());
+			LOG.info("Error Code: " + e.getErrorCode());
+			LOG.info("Error Message: " + e.getErrorMessage());
+		}
+	}
 }
-
-
